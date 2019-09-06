924605bd655715e618ccf95e8981941b3c6c1a14
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2019-09-07 01:10:41 +0530

    Harris corners function

--- random/image_class.py
+++ random/image_class.py
@@ -11,6 +11,15 @@ import cv2
 To be imported in driver
 """
 
+GAUSS_KERNEL_SIZE = 5
+
+HARRIS_BLOCK_SIZE = 2
+HARRIS_KSIZE = 5 #Aperture for sobel operator
+HARRIS_K = 0.07
+HARRIS_THRESHOLD = 0.01
+
+
+
 class Image:
 
 	"""
@@ -21,15 +30,39 @@ class Image:
 	def __init__(self, np_matrix):
 		self.img = np_matrix
 
+	def display(self, name="Output", image = self.img):
+
+		""" Display Image """
+		
+		cv2.imShow(name, image)
+
+		#Temporary part - waitkey:
+		if cv2.waitKey(0):
+			cv2.destroyAllWindows()
+
+
 	def replace(self, new_img):
 		self.img = new_img
 
-	def gaussian_blur(self, kernel_size=5):
+	
+	def gaussian_blur(self, kernel_size=GAUSS_KERNEL_SIZE):
 		ret_img = cv2.blur(self.img, (kernel_size, kernel_size))
 		return ret_img
 
+	
 	def gaussian_diff(self, kernel_size=5):
-		return img - self.gaussian_blur(kernel_size)
+		return self.img - self.gaussian_blur(kernel_size)
 
-	def harrison_corners(self):
-		pass
+	
+	def harris_corners(self):
+		temp_img = np.float32(cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY))
+		ret_img = cv2.cornerHarris(temp_img, blockSize = HARRIS_BLOCK_SIZE, HARRIS_KSIZE, HARRIS_K)
+		ret_img = cv2.dilate(ret_img, None)
+
+		#Currently returning original image with highlighted corners
+		#May need to be changed for returning only greyscale with threhold
+
+		modified_img = self.img
+		modified_img[ret_img > HARRIS_THRESHOLD*ret_img.max()] = [0,0,255]
+
+		return modified_img
